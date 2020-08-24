0c19a38ec258f4ed59565db9c89eee940874e9b2
etaek <dmsxodor2@naver.com>
2020-08-24 13:36:43 +0900

    기본 셋팅

---
+++ src/main/java/com/euntaek/BestSellerSpring/BestSellerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.euntaek.BestSellerSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestSellerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestSellerSpringApplication.class, args);
+	}
+
+}
